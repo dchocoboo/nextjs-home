d2c084173eb6a9b3fd0cb7de4754fb94af068424
lawwman <lawrence_koh@tech.gov.sg>
2023-04-26 17:55:05 +0800

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
+
+const BLOG_URL = 'http://localhost:4000'
+
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: `/:path*`,
+      },
+      {
+        source: "/blog",
+        destination: `${BLOG_URL}/blog`,
+      },
+      {
+        source: "/blog/:path*",
+        destination: `${BLOG_URL}/blog/:path*`,
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
